5f710305cb8f6d25ecbb2d58a0e56b8799dd6837
javilopezleza <javilopezleza90@gmail.com>
2023-08-16 19:13:01 +0200

    Final de seccion

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { Character } from '../interfaces/character.interface';
+import { Character } from '../interfaces/character.interface';
 import { DbzService } from '../services/dbz.service';
 
 @Component({
@@ -9,6 +9,17 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPageComponent {
 
-    constructor(public dbzService: DbzService) {}
+    constructor( private dbzService: DbzService) {}
 
+    get characters(): Character[] {
+        return [...this.dbzService.characters];
+    }
+
+    onDeleteCharacter(id: string):void{
+        this.dbzService.deleteCharacterById(id);
+    }
+
+    onNewCharacter(character: Character):void {
+        this.dbzService.addCharacter(character);
+    }
 }
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({ //Decorador que le dice a Angular que esto es un servicio
@@ -9,26 +9,36 @@ export class DbzService {
 
 
     public characters: Character[] = [{
-        name:'Krilin',
-        power:1000
-    },{
+        id: uuid(),
+        name: 'Krilin',
+        power: 1000
+    }, {
+        id: uuid(),
         name: 'Goku',
         power: 9500
-    },{
+    }, {
+        id: uuid(),
         name: 'Vegeta',
         power: 7500
     }
 
-];
+    ];
 
-    onNewCharacter(character:Character):void{
-       this.characters.push(character);
+    addCharacter(character: Character): void {
+
+        const newCharacter: Character = {id: uuid(), ...character };
+
+        this.characters.push(newCharacter);
     }
-    
-    onDeleteCharacter(index:number){
-        this.characters.splice(index,1);
+
+    // onDeleteCharacter(index: number) {
+    //     this.characters.splice(index, 1);
+    // }
+
+
+    deleteCharacterById(id:string){
+        this.characters = this.characters.filter(character => character.id !== id);
     }
-    
 
 
 
